38c571378e267b9d9181e5375e5d5fcb5bdf88d9
absomato <absomato@naver.com>
2019-08-15 14:27:37 +0900

    final

--- src/no2798블랙잭/Main.java
+++ src/no2798블랙잭/Main.java
@@ -1,4 +1,27 @@
 package no2798블랙잭;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt(),m=sc.nextInt(),i,j,k,max=-9;
+        int[] p= new int[n];
+        for(i=0;i<n;i++){
+            p[i]=sc.nextInt();
+
+        }
+        Arrays.sort(p);
+        for(i=n-1;i>=2;i--){
+            for(j=i-1;j>=1;j--){
+                for(k=j-1;k>=0;k--){
+                    if(p[i]+p[j]+p[k]<=m) {
+                        if(max<p[i]+p[j]+p[k])max=p[i]+p[j]+p[k];
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
 }
