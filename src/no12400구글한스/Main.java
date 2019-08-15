d9413d61835bd1a3b3103c66f4d23b44528ab4aa
absomato <absomato@naver.com>
2019-08-15 14:25:57 +0900

    google hanse first version

--- src/no12400구글한스/Main.java
+++ src/no12400구글한스/Main.java
@@ -1,4 +1,22 @@
 package no12400구글한스;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i,n=sc.nextInt(),j;
+        char[] p = new char[27];
+
+
+        for(i=0;i<=n;i++) {
+            String a = sc.nextLine();
+            String s = " ";
+            for (j = 0; j < a.length(); j++) {
+                if(s.equals(a.charAt(j))) System.out.print(" ");
+                else System.out.print(a.charAt(j)-'a');
+            }
+            System.out.println();
+        }
+    }
 }
