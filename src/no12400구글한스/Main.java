9613a10d393006f81229f8350590ab46615e6258
absomato <absomato@naver.com>
2019-09-15 22:03:13 +0900

    solution

--- src/no12400구글한스/Main.java
+++ src/no12400구글한스/Main.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int i,n=sc.nextInt(),j;
-        char[] p = new char[27];
+        int i,j;
+        int n=sc.nextInt();
+        sc.nextLine();
+        String[] a = new String[n+1];
+        int[] change = {25,8,5,19,15,3,22,24,4,21,9,7,12,2,11,18,26,20,14,23,10,16,6,13,1,17};
 
-
-        for(i=0;i<=n;i++) {
-            String a = sc.nextLine();
-            String s = " ";
-            for (j = 0; j < a.length(); j++) {
-                if(s.equals(a.charAt(j))) System.out.print(" ");
-                else System.out.print(a.charAt(j)-'a');
-            }
-            System.out.println();
-        }
+for(i=1;i<=n;i++) {
+    a[i] = sc.nextLine();
+}
+for(i=1;i<=n;i++){
+    System.out.print("Case #"+i+": ");
+    for (j = 0; j < a[i].length(); j++) {
+        if ((a[i].charAt(j) - 'a') >= 0 && (a[i].charAt(j) - 'a') <= 26)
+            System.out.print((char) (change[(a[i].charAt(j) - 'a')] + 96));
+        else System.out.print(" ");
+    }
+    System.out.println();
+}
     }
 }
